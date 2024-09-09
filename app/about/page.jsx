c01b2777c1ca01a9ee4812d8bdd0a73ea4b83fc3
emma ezeiru <ezeiruemma5t@gmail.com>
2024-09-09 12:24:56 +0100

    about page

---
+++ app/about/page.jsx
@@ -0,0 +1,20 @@
+export const About = () => {
+  return (
+    <div>
+        <div class="container">
+            <h2>About Me</h2>
+            <div>
+                <h4>A bit about me</h4>
+                <p>I'm a self-taught web developer and Mobile App Developer with experience in designing new features from ideation to production, implementation of wireframes and design flows into high performance software applications. I take into consideration the user experience while writing reusable and efficient code. I passionately combine good design, technology, and innovation in all my projects, which I like to accompany from the first idea to release.Currently, I'm focused on the backend development.</p>
+            </div>
+            <div>
+                <h4>Technologies and Tools</h4>
+                <p>Using a combination of cutting-edge technologies and reliable open-source software I build user-focused, performant apps and websites for smartphones, tablets, and desktops.</p>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+  )
+}
