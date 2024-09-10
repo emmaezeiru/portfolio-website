d03c3c56151ef9d846b1aa447a0970dd497e3c37
emma ezeiru <ezeiruemma5t@gmail.com>
2024-09-10 12:24:56 +0100

    home page

--- app/page.jsx
+++ app/page.jsx
@@ -25,20 +25,6 @@ export default function Home() {
       </main>
       <div>
         
-        <div>
-          <div class="container">
-            <h2>Services</h2>
-            <div>
-              <h4>What i provide</h4>
-            </div>
-            <div class="card">
-              <div>
-                
-              </div>
-            </div>
-          </div>
-        </div>
-        
     </div>
     </div>
   );
