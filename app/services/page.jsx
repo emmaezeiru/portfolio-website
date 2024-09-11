e695abbff3776a1a2eb29ac1b5149266fa52c1eb
emma ezeiru <ezeiruemma5t@gmail.com>
2024-09-11 12:24:56 +0100

    service page

---
+++ app/services/page.jsx
@@ -0,0 +1,19 @@
+export const Service = () => {
+  return (
+    <div>
+        <div>
+          <div class="container">
+            <h2>Services</h2>
+            <div>
+              <h4>What i provide</h4>
+            </div>
+            <div class="card">
+              <div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
