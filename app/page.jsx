7355b726393bc9bf3b0db281deeda9875c17c13b
emma ezeiru <ezeiruemma5t@gmail.com>
2024-09-17 12:24:56 +0100

    update: home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from 'framer-motion ';
 
 export default function Home() {
   return (
     <div>
-      <main>
-        <div class="container">
-          <div>
-            <h1>Hi, I am Emma</h1>
-            <p>I am a backend / Full-Stack Developer</p>
-          </div>
+      <main class="mx-auto max-w-7xl px-4 sm:px-6 md:mt-0 lg:px-8 flex flex-col md:flex-row items-center justify-center md:justify-between h-screen"
+        id="/"
+      >
+        <div class="sm:text-center lg:text-left">
+          <h1 class="text-4xl tracking-tight font-extrabold text-black sm:text-5xl md:text-6xl">
+            Hi, I am Emma
+          </h1>
+          <p>I am a backend / Full-Stack Developer</p>
           <div>
             <Link href="#">
             </Link>
