2cfabad5e9580c66c8ca86b0535b1de5c8318628
emma ezeiru <ezeiruemma5t@gmail.com>
2024-12-05 00:23:45 +0100

    added links to constants

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -15,12 +15,12 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-white border-gray-200 z-50 shadow-lg md:px-10 px-1 fixed w-full top-0">
-      <div className="flex justify-between items-center py-2 md:py-4 md:px-2 pl-2 mx-auto">
-        <div className="flex items-center cursor-pointer">
+    <nav class="bg-white border-gray-200 z-50 shadow-lg md:px-10 px-1 fixed w-full top-0">
+      <div class="flex justify-between items-center py-2 md:py-4 md:px-2 pl-2 mx-auto">
+        <div class="flex items-center cursor-pointer">
           <Link 
             href="/" 
-            className="font-bold text-xl"
+            class="font-bold text-xl"
             scroll={true}
           >
             Emma Ezeiru
@@ -28,13 +28,13 @@ const Navbar = () => {
         </div>
 
         {/* Desktop Navigation */}
-        <div className="hidden md:flex items-center space-x-8">
-          <ul className="flex flex-row space-x-8 text-md font-medium">
+        <div class="hidden md:flex items-center space-x-8">
+          <ul class="flex flex-row space-x-8 text-md font-medium">
             {links.map((el) => (
               <li key={el.name}>
                 <Link href={`#${el.id}`}
                   scroll={true}
-                  className="block py-2 px-3 text-black hover:bg-blue-500 hover:text-white rounded-md"
+                  class="block py-2 px-3 text-black hover:bg-blue-500 hover:text-white rounded-md"
                 >
                   {el.name}
                 </Link>
@@ -46,26 +46,26 @@ const Navbar = () => {
         {/* Mobile Menu Toggle */}
         <button
           onClick={() => setToggle(!toggle)}
-          className="md:hidden flex items-center focus:outline-none pr-3"
+          class="md:hidden flex items-center focus:outline-none pr-3"
           aria-label="Toggle menu"
         >
-          <div className="space-y-1.5">
-            <div className="w-6 h-0.5 bg-black"></div>
-            <div className="w-6 h-0.5 bg-black"></div>
-            <div className="w-6 h-0.5 bg-black"></div>
+          <div class="space-y-1.5">
+            <div class="w-6 h-0.5 bg-black"></div>
+            <div class="w-6 h-0.5 bg-black"></div>
+            <div class="w-6 h-0.5 bg-black"></div>
           </div>
         </button>
       </div>
 
       {/* Mobile Dropdown Menu */}
       {toggle && (
-        <div className="md:hidden bg-white shadow-lg rounded-lg mt-2 py-2 px-3 fixed top-16 right-2 w-48 z-50">
-          <ul className="space-y-2">
+        <div class="md:hidden bg-white shadow-lg rounded-lg mt-2 py-2 px-3 fixed top-16 right-2 w-48 z-50">
+          <ul class="space-y-2">
             {links.map((el) => (
               <li key={el.name} onClick={() => setToggle(false)}>
                 <Link href={`#${el.id}`}
                   scroll={true}
-                  className="block py-2 px-3 text-black hover:bg-blue-500 hover:text-white rounded-md"
+                  class="block py-2 px-3 text-black hover:bg-blue-500 hover:text-white rounded-md"
                 >
                   {el.name}
                 </Link>
--- app/constants.jsx
+++ app/constants.jsx
@@ -3,31 +3,35 @@ export const techStack = [
     { name: "C Language", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-plain.svg" },
   
     { name: "TypeScript", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" },
+    { name: "NodeJS", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" },
     { name: "Express", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" },
   
-    { name: "NodeJS", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" },
     { name: "Postman", link: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" },
   
-    { name: "Dart", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" },
-    { name: "Flutter", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-plain.svg" },
-    { name: "React Native", link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png" },
+    //{ name: "Dart", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg" },
+    //{ name: "Flutter", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-plain.svg" },
+    //{ name: "React Native", link: "https://pagepro.co/blog/wp-content/uploads/2020/03/react-native-logo-884x1024.png" },
     { name: "HTML", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" },
     { name: "CSS", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" },
-    { name: "Bootstrap", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" },
-    { name: "Redux", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" },
-    { name: "Sass", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" },
-  
+    { name: "Next", link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" },
+    { name: "React", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" },
     { name: "Javascript", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
     { name: "Tailwind CSS", link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" },
-    { name: "React", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" },
-    { name: "MySQl", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-plain.svg" },
-    { name: "Mongo DB", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg" },
-    { name: "Heroku", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" },
-    // { name: "Azure", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain.svg" },
-    { name: "Git", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" },
-    // { name: "Docker", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-plain.svg" },
-    { name: "Firebase", link: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" },
+    { name: "Python", link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" },
+    //{ name: "Bootstrap", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-plain.svg" },
+    //{ name: "Redux", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg" },
+    { name: "Sass", link: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" },
   
+
+    { name: "MySQl", link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" },
+    { name: "Mongo DB", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-plain.svg" },
+    //{ name: "Heroku", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" },
+    // { name: "Azure", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-plain.svg" },
+    //{ name: "Firebase", link: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" },
+    { name: "Postgresql", link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" },
+    { name: "Docker", link: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original-wordmark.svg" },
+    { name: "Git", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" },
+    
     // { name: "Bash", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" },
     // { name: "Vim", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg" },
   
@@ -36,10 +40,11 @@ export const techStack = [
   ]
   
   
-  export const contactLinks = [{ name: 'Stack Overflow', url: 'https://img.icons8.com/external-tal-revivo-color-tal-revivo/40/000000/external-stack-overflow-is-a-question-and-answer-site-for-professional-logo-color-tal-revivo.png', link: 'https://stackoverflow.com/users/11788531/aakash-sharma' },
-  { name: 'Instagram', url: 'https://img.icons8.com/doodle/40/000000/instagram-new--v2.png', link: 'https://www.instagram.com/aakashsh1999/' },
-  { name: 'Github', url: 'https://img.icons8.com/doodle/40/000000/github--v1.png', link :'https://github.com/aakashsh1999'},
-  { name: 'LinkedIn', url: 'https://img.icons8.com/doodle/40/000000/linkedin--v2.png', link: 'https://www.linkedin.com/in/aakashsh1999/'}
+  export const contactLinks = [{ name: 'Stack Overflow', url: 'https://img.icons8.com/external-tal-revivo-color-tal-revivo/40/000000/external-stack-overflow-is-a-question-and-answer-site-for-professional-logo-color-tal-revivo.png', link: 'https://stackoverflow.com/users/19101534/ezeiruemma' },
+  { name: 'Instagram', url: 'https://img.icons8.com/doodle/40/000000/instagram-new--v2.png', link: 'https://www.instagram.com/sa.int__/' },
+  { name: 'Github', url: 'https://img.icons8.com/doodle/40/000000/github--v1.png', link :'https://github.com/emmaezeiru'},
+  { name: 'LinkedIn', url: 'https://img.icons8.com/doodle/40/000000/linkedin--v2.png', link: 'https://www.linkedin.com/in/chukwubuikem-ezeiru-elias-538871296'},
+  { name: 'X', url: 'https://img.icons8.com/?size=100&id=phOKFKYpe00C&format=png&color=000000', link: 'https://www.x.com/emmaezeiru/'}
   ]
   
   
@@ -51,15 +56,6 @@ export const techStack = [
       img: "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-development-coding-kiranshastry-lineal-color-kiranshastry.png"
     },
     {
-      name: 'Mobile Application Development',
-      desc: `We provide a range of mobile application development services
-      including custom mobile development on Android platforms, building
-      cross-platform apps, designing user experience and integrating
-      novel mobile interfaces such as chat and voice`,
-      img: "https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-app-development-responsive-web-design-justicon-lineal-color-justicon.png"
-    },
-  
-    {
       name: 'Backend Development',
       desc: `Graphic design is a craft where professionals create visual
       content to communicate messages. By applying visual hierarchy and
@@ -68,6 +64,12 @@ export const techStack = [
       elements in interactive designs, to optimize the user experience.`,
       img: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png"
     },
-  
-  
+    {
+      name: 'Frontend Development',
+      desc: `I provide a range of frontend development services
+      including creating responsive user interfaces, building dynamic web applications,
+      designing seamless user experiences, and integrating modern tools and libraries 
+      to enhance functionality and performance.`,
+      img: "https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-app-development-responsive-web-design-justicon-lineal-color-justicon.png"
+    }  
   ]
